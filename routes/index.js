abfc8982e0da1f7b649afc1bed71d22c64b5be0f
hepdad <hepdad@gmail.com>
2015-07-13 20:34:46 +0200

    quiz version creditos

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
